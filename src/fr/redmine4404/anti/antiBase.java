dd2d61edfa1518c4734bd9f1b53103c2827cfa4c
redmine <redmine4404@gmail.com>
2022-07-25 16:57:06 +0200

    add init file

---
+++ src/fr/redmine4404/anti/antiBase.java
@@ -0,0 +1,10 @@
+package fr.redmine4404.anti;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class antiBase extends JavaPlugin{
+	@Override
+    public void onEnable() {
+		System.out.println("antibase Enable");
+    }
+}
